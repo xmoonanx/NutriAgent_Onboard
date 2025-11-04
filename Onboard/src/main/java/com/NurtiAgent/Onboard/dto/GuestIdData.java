043dbe1a642e2fa8abd867a0b2d190a6a26cdd91
xmoonanx <xmoonanx@gmail.com>
2025-11-04 11:12:03 +0900

    docs: guestidservice 삭제

--- Onboard/src/main/java/com/NurtiAgent/Onboard/dto/GuestIdData.java
+++ Onboard/src/main/java/com/NurtiAgent/Onboard/dto/GuestIdData.java
@@ -1,4 +1,7 @@
 package com.NurtiAgent.Onboard.dto;
 
-public record GuestIdData(String guestId) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "게스트 아이디 정보")
+public record GuestIdData(@Schema(description = "게스트 아이디") String guestId) {
 }
